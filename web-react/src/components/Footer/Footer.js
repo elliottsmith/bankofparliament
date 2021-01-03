20866e15fcc8c8889f720224ab9e5a7d4efeea8d
elliott <elliott@elliotts-MacBook-Pro.local>
2021-01-03 23:07:53 +0000

    Updating app footer

--- web-react/src/components/Footer/Footer.js
+++ web-react/src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ export default function Footer(props) {
     <footer className={classes.footer}>
       <div className={classes.container}>
         <div className={classes.left}>
-          <List className={classes.list}>
+          {/* <List className={classes.list}>
             <ListItem className={classes.inlineBlock}>
               <a href="#home" className={classes.block}>
                 Home
@@ -37,19 +37,18 @@ export default function Footer(props) {
                 Blog
               </a>
             </ListItem>
-          </List>
+          </List> */}
         </div>
         <p className={classes.right}>
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
             <a
-              href="https://www.creative-tim.com?ref=mdr-footer"
+              href="https://github.com/elliottsmith/bankofparliament"
               target="_blank"
               className={classes.a}
             >
-              Creative Tim
+              Bank of Parliament
             </a>
-            , made with love for a better web
           </span>
         </p>
       </div>
